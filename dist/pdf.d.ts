7b891ea533da4dd1d9ff26d5a571bf4d873b3e0e
AgentNoon239 <tom@maldenmail.co.uk>
2025-05-16 19:24:14 +0100

    rebuilding

--- dist/pdf.d.ts
+++ dist/pdf.d.ts
@@ -34,8 +34,11 @@ export declare class PDF {
     computePadding(config?: PaddingConfig): number[];
     addParagraph(text: string, config?: TextConfig & PaddingConfig): void;
     addRichText(text: RichText, baseConfig: TextConfig & PaddingConfig): void;
-    addHead(image?: string, url?: string): void;
+    addHead(image?: HTMLImageElement, url?: string, width?: number): void;
     insertBar(value: number, maxValue: number, topLeft: [number, number], width: number, config?: PaddingConfig): [number, () => void];
+    addHR(baseConfig?: PaddingConfig & {
+        color: string;
+    }): void;
     richTextLayout(text: RichText, topLeft: [number, number], width: number, baseConfig?: TextConfig): InsertTextParams[];
     insertText(text: string, topLeft: [number, number], width: number, config?: TextConfig & PaddingConfig): [number, () => void];
     addTable(contents: Array<Array<(positioning: [number, number, number]) => [number, () => void]>>, widths: number[], config?: any): void;
